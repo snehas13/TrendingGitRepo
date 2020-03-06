7e4f1f3decedd4c7eb54e35411612263f5deeac7
snehas13 <sneha.s13@gmail.com>
2020-03-07 00:21:40 +0530

    adding domain layer

---
+++ domain/src/main/java/com/learn/domain/BuiltBy.kt
@@ -0,0 +1,9 @@
+package com.learn.domain
+
+data class BuiltBy (
+
+    val username : String,
+    val href : String,
+    val avatar : String
+
+)
---
+++ domain/src/main/java/com/learn/domain/GitRepo.kt
@@ -0,0 +1,17 @@
+package com.learn.domain
+
+data class GitRepo(
+
+    val author : String,
+    val name : String,
+    val avatar : String,
+    val url : String,
+    val description : String,
+    val language : String,
+    val languageColor : String,
+    val stars : Int,
+    val forks : Int,
+    val currentPeriodStars : Int,
+    val builtBy : List<BuiltBy>
+
+)
