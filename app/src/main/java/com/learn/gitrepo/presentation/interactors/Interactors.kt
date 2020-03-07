a971ed996fbd3870e6618444abb9012627d4f9f6
snehas13 <sneha.s13@gmail.com>
2020-03-07 17:59:57 +0530

    removed room specific data source impl

--- app/src/main/java/com/learn/gitrepo/presentation/interactors/Interactors.kt
+++
@@ -1,9 +0,0 @@
-package com.learn.gitrepo.presentation.interactors
-
-import com.learn.usecases.AddGitRepos
-import com.learn.usecases.GetGitRepos
-
-data class Interactors(
-    val addGitRepos: AddGitRepos,
-    val getGitRepos: GetGitRepos
-)
