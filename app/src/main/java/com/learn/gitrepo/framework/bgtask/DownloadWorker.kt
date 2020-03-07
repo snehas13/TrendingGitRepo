de0a58f758fe409da53cbde845102371d86951b6
snehas13 <sneha.s13@gmail.com>
2020-03-07 17:53:19 +0530

    Added coroutineWorker for background task

---
+++ app/src/main/java/com/learn/gitrepo/framework/bgtask/DownloadWorker.kt
@@ -0,0 +1,41 @@
+package com.learn.gitrepo.framework.bgtask
+
+import android.content.Context
+import android.util.Log
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.learn.data.ServerCallback
+import com.learn.domain.GitRepo
+import com.learn.gitrepo.framework.interactors.Interactors
+import kotlinx.coroutines.*
+import org.koin.standalone.KoinComponent
+import org.koin.standalone.inject
+
+class DownloadWorker(context : Context, workerParams : WorkerParameters) : CoroutineWorker(context, workerParams), KoinComponent {
+
+    val interactors :  Interactors by inject()
+    val TAG : String = "DownloadWorker"
+
+    override suspend fun doWork(): Result = coroutineScope {
+        val jobs = async {
+
+            interactors.fetchGitRepos(object : ServerCallback {
+                override fun onError() {
+                    Log.e(TAG,"Error downloading the Gitrepo")
+                }
+
+                override fun onRepoListLoaded(repoList: List<GitRepo>) {
+                    GlobalScope.launch {
+                        withContext(Dispatchers.IO) {
+                            interactors.addGitRepos(repoList)
+                        }
+                    }
+
+                }
+            })
+        }
+
+        jobs.await()
+        Result.success()
+    }
+}
